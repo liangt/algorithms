bbe633c7ee2e77b687c63ac5f9f507b4d3c66608
Tan Liang <1017086816@qq.com>
2015-04-13 21:19:17 +0800

    Modify RegularExpression.cpp

--- RegularExpression.cpp
+++ RegularExpression.cpp
@@ -83,109 +83,163 @@ struct state{
 	int ch;                // 0-127: ASCII字符, 128: 空转移(分支), 129: 终止状态
 	state* out1;   //  分支1
 	state* out2;   //  分支2
+	state(): ch(-1), out1(NULL), out2(NULL){}
 	state(int ch, state* out1=NULL, state* out2=NULL):ch(ch), out1(out1), out2(out2){}
 };
 
 // NFA片段
-struct fraq{
+struct frag{
 	state* start;         // 开始状态
-	list<state> out; // 没有任何指向其他状态的状态
-	fraq(char c): start(new state(c)){}
+	list<state*> out; // 没有任何指向其他状态的状态
+	frag(): start(NULL){}
+	frag(char c): start(new state(c)){}
+	frag(state* s, list<state*> o): start(s), out(o){}
 	bool operator!=(int c){
 	    return start->ch != c;
 	}
 };
 
-void linkOperator(string re, int n, int i, fraq tmp, stack<fraq>& ans, stack<char>& ops){
-    fraq tmp1;
+void linkOperator(string re, int n, int i, frag nexttop, stack<frag>& nfafrag, stack<char>& ops){
+    frag tmp;
     if(i!=n-1 && re[i+1]!='*' && re[i+1]!='+' && re[i+1]!='?' && re[i+1]!='|' && re[i+1]!=')'){
-        while(!ans.empty() && ans.top()!='(' && !ops.empty() && ops.top()=='#'){
-            tmp1 = ans.top() ;
-            ans.pop();
-            tmp1.out.clear();
-            tmp1.out.splice(tmp1.out.end(), tmp.out);
+        while(!nfafrag.empty() && nfafrag.top()!='(' && !ops.empty() && ops.top()=='#'){
+            tmp = nfafrag.top() ;
+            nfafrag.pop();
+             // 将tmp的输出状态指向nexttop的开始状态
+            for(list<state*>::iterator it=tmp.out.begin(); it!=tmp.out.end(); it++)
+                (*it)->out1 = nexttop.start;
+            //将tmp的输出状态重置为nexttop的输出状态
+            tmp.out.clear();
+            tmp.out.splice(tmp.out.end(), nexttop.out);
             ops.pop();   // delete #
-            tmp = tmp1;
+            nexttop = tmp;
         }
-        ans.push(tmp);
+        nfafrag.push(nexttop);
         ops.push('#');
     }
     else
-        ans.push(tmp);
+        nfafrag.push(nexttop);
 }
 
-state* re2nfa(string str){
-	int n = str.size();
-	fraq tmp1, tmp2;
-	state* s;
+state* re2nfa(string re){
+	int n = re.size();
+	frag tmp1, tmp2;
+	state *s;
 	stack<char> ops;
-	stack<fraq> nfafraq;
+	stack<frag> nfafrag;
 	for(int i=0; i<n; i++){
         switch(re[i]){
             case '*':
+                tmp1 = nfafrag.top();
+                nfafrag.pop();
+                s = new state(128, tmp1.start, new state());
+                for(list<state*>::iterator it=tmp1.out.begin(); it!=tmp1.out.end(); it++)
+                    (*it)->out1 = s;
+                tmp1.out.clear();
+                tmp1.out.push_back(s->out2);
+                nfafrag.push(tmp1);
                 break;
             case '?':
+                tmp1 = nfafrag.top();
+                nfafrag.pop();
+                s = new state(128, tmp1.start, new state());
+                tmp1.out.push_back(s->out2);
+                nfafrag.push(tmp1);
                 break;
             case '+':
+                tmp1 = nfafrag.top();
+                nfafrag.pop();
+                s = new state(128, tmp1.start, new state());
+                for(list<state*>::iterator it=tmp1.out.begin(); it!=tmp1.out.end(); it++)
+                    (*it)->out1 = s;
+                tmp1.out.clear();
+                tmp1.out.push_back(s->out2);
+                nfafrag.push(tmp1);
                 break;
             case '|':
-                tmp1 = ans.top();
-                ans.pop();
-                while(ans.top().ch!='(' && !ops.empty()){
-                    tmp2 = ans.top();
-                    ans.pop();
+                tmp1 = nfafrag.top();
+                nfafrag.pop();
+                while(nfafrag.top()!='(' && !ops.empty()){
+                    tmp2 = nfafrag.top();
+                    nfafrag.pop();
                     if(ops.top() == '#'){   // #
+                        // 将tmp2的输出状态指向tmp1的开始状态
+                        for(list<state*>::iterator it=tmp2.out.begin(); it!=tmp2.out.end(); it++)
+                            (*it)->out1 = tmp1.start;
+                         //将tmp2的输出状态重置为tmp1的输出状态
                         tmp2.out.clear();
                         tmp2.out.splice(tmp2.out.end(), tmp1.out);
                     }
                     else{  // |
-                        s = new state(128, tmp2.start, tmp1.start);
-                        tmp2.out.splice(tmp2.out.end(), tmp1.out);
+                        s = new state(128, tmp2.start, tmp1.start);   // 创建新的开始状态指向tmp2和tmp1的开始状态
+                        tmp2.out.splice(tmp2.out.end(), tmp1.out);   // 合并tmp2和tmp1的输出状态
                         tmp2.start = s;
                     }
                     ops.pop();
                     tmp1 = tmp2;
                 }
-                ans.push(tmp1);
+                nfafrag.push(tmp1);
                 ops.push('|');
                 break;
             case '(':
-                ans.push(frag('(');
+                nfafrag.push(frag('('));
                 break;
             case ')':
-                tmp = ans.top();
-                ans.pop();
-                while(ans.top()!="("){
-                    tmp = ans.top() + tmp + ops.top();
-                    ans.pop();
+                tmp1 = nfafrag.top();
+                nfafrag.pop();
+                while(nfafrag.top()!='('){
+                    tmp2 = nfafrag.top();
+                    nfafrag.pop();
+                    if(ops.top() == '#'){   // #
+                        // 将tmp2的输出状态指向tmp1的开始状态
+                        for(list<state*>::iterator it=tmp2.out.begin(); it!=tmp2.out.end(); it++)
+                            (*it)->out1 = tmp1.start;
+                         //将tmp2的输出状态重置为tmp1的输出状态
+                        tmp2.out.clear();
+                        tmp2.out.splice(tmp2.out.end(), tmp1.out);
+                    }
+                    else{  // |
+                        s = new state(128, tmp2.start, tmp1.start);   // 创建新的开始状态指向tmp2和tmp1的开始状态
+                        tmp2.out.splice(tmp2.out.end(), tmp1.out);   // 合并tmp2和tmp1的输出状态
+                        tmp2.start = s;
+                    }
                     ops.pop();
+                    tmp1 = tmp2;
                 }
-                ans.pop();  // delete (
-                addLinkOperator(re, n, i, tmp, ans, ops);
+                nfafrag.pop();  // delete (
+                linkOperator(re, n, i, tmp1, nfafrag, ops);
                 break;
             default:
-                tmp = re[i];
-                addLinkOperator(re, n, i, tmp, ans, ops);
+                linkOperator(re, n, i, re[i], nfafrag, ops);
         }
     }
-    tmp = ans.top();
-    ans.pop();
+    tmp1 = nfafrag.top();
+    nfafrag.pop();
     while(!ops.empty()){
-        tmp = ans.top() + tmp +ops.top();
-        ans.pop();
+        tmp2 = nfafrag.top();
+        nfafrag.pop();
+        if(ops.top() == '#'){   // #
+            // 将tmp2的输出状态指向tmp1的开始状态
+            for(list<state*>::iterator it=tmp2.out.begin(); it!=tmp2.out.end(); it++)
+                (*it)->out1 = tmp1.start;
+                //将tmp2的输出状态重置为tmp1的输出状态
+                tmp2.out.clear();
+                tmp2.out.splice(tmp2.out.end(), tmp1.out);
+        }
+        else{  // |
+            s = new state(128, tmp2.start, tmp1.start);   // 创建新的开始状态指向tmp2和tmp1的开始状态
+            tmp2.out.splice(tmp2.out.end(), tmp1.out);   // 合并tmp2和tmp1的输出状态
+            tmp2.start = s;
+        }
         ops.pop();
+        tmp1 = tmp2;
     }
-    return tmp;
+    return tmp1.start;
 }
 
 int main(){
     string in;
     cin>>in;
-    fraq fg('a');
-    for(int i=0; i<in.size(); i++)
-        if(fg != in[i])
-            cout<<"Not equal"<<endl;
-        else
-            cout<<"Equal"<<endl;
+    re2nfa(in);
 	return 0;
 }
