d46f2ae92c7810a747cca4168c62acb650afd5c4
Tan Liang <1017086816@qq.com>
2015-09-21 22:25:14 +0800

    bit_set

---
+++ programming_pearls/bit_set.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class bit_set{
+public:
+    bit_set(int n): n(n){
+        int m = n >> 3;
+        if(n & 7)
+            m ++;
+        bits = new int [m];
+    }
+    ~bit_set(){ delete [] bits; }
+    void reset(int pos){
+    }
+private:
+    int n;
+    unsigned *bits;
+};
+
+int main(){
+    return 0;
+}
